69caaca5d942e4280011119d8760bca2463b7bdf
darisrd <daris.raihan@gmail.com>
2021-10-21 16:14:07 +0700

    bentar

--- sirenMTR/proses/motor-input-proses.php
+++ sirenMTR/proses/motor-input-proses.php
@@ -5,19 +5,12 @@ $nama_motor=$_POST['nama_motor'];
 $merk_motor=$_POST['merk_motor'];
 $status="Tersedia";
 	
-// if(isset($_POST['simpan'])){
-// 	mysqli_query(
-// 		"INSERT INTO tbbuku
-// 		VALUES('$id_buku','$judul_buku','$kategori','$pengarang','$penerbit','$status')"
-// 	);
-// 	header("location:../index.php?p=buku");
-// }
 
 if(isset($_POST['simpan'])){
 
 	$sql = 
-	"INSERT INTO tbbuku
-	VALUES('$id_buku','$judul_buku','$kategori','$pengarang','$penerbit','$status')";
+	"INSERT INTO tbmotor
+	VALUES('$id_motor','$nama_motor','$merk_motor','$status')";
 	$query = mysqli_query($db, $sql);
 
 	header("location:../pagez/buku/indexB.php");
